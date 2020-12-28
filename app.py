7f9b72e756933608278430c3639786cfb5dc81bb
rainbowCat <jazziblue@gmail.com>
2020-12-27 19:19:19 -0800

    set up navbar

--- app.py
+++ app.py
@@ -5,9 +5,19 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/index")
+# @app.route("/index")
 def index():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/auditions")
+def faq():
+    return render_template("auditions.html")
 
 
 if __name__ == "__main__":

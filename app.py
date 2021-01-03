4f8e5c64f47ebb084a8872a4dc0a501dba57e493
rainbowCat <jazziblue@gmail.com>
2021-01-02 23:39:26 -0800

    add page links

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 app = Flask(__name__)
 
+
 @app.route("/")
 # @app.route("/index")
 def index():
@@ -16,9 +17,29 @@ def about():
 
 
 @app.route("/auditions")
-def faq():
+def auditions():
     return render_template("pages/auditions.html")
 
 
+@app.route("/more")
+def more():
+    return render_template("pages/more.html")
+
+
+@app.route("/test/no-sidebar")
+def test0():
+    return render_template("pages/no-sidebar.html")
+
+
+@app.route("/test/left-sidebar")
+def test1():
+    return render_template("pages/left-sidebar.html")
+
+
+@app.route("/test/right-sidebar")
+def test2():
+    return render_template("pages/right-sidebar.html")
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)  # TODO set false when deploy

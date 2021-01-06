617f1cd187a1021a9838e53ba05e861db225aa43
rainbowCat <jazziblue@gmail.com>
2021-01-06 11:19:38 -0800

    add sidenav

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,30 @@ $(".back-to-top").click(function () {
   );
   return false;
 });
+
+/*--------------------------------------------------------------
+# Side nav
+--------------------------------------------------------------*/
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("body").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("body").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}

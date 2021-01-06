182321b6f43fd7400544833d86026667d86a6ab0
rainbowCat <jazziblue@gmail.com>
2021-01-06 13:36:30 -0800

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ function closeNav() {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.body.style.marginLeft = "250px";
+  document.header.style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 

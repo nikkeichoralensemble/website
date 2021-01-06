ea0a2305562d1977bb96ead64ec9a1b6c0db82a0
rainbowCat <jazziblue@gmail.com>
2021-01-05 21:09:38 -0800

    add back-to-top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,23 @@
 /// shitf main section down
 topHeight = $("header").outerHeight(true);
 $("main").css({ "margin-top": topHeight + "px" });
+
+// Back to top button
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $(".back-to-top").fadeIn("slow");
+  } else {
+    $(".back-to-top").fadeOut("slow");
+  }
+});
+
+$(".back-to-top").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: 0,
+    },
+    1500,
+    "easeInOutExpo"
+  );
+  return false;
+});

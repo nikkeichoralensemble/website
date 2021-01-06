3892ae32f27bc9db0272b70f26e917e70df2819e
rainbowCat <jazziblue@gmail.com>
2021-01-06 12:37:19 -0800

    organize

--- assets/js/navbar.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  $("body").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  $("body").style.marginLeft = "0px";
-}

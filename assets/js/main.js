1d8ae81bab6fe302333be86dce4e33faa1ff2cb6
rainbowCat <jazziblue@gmail.com>
2021-01-06 20:15:32 -0800

    fix sidenav javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,28 +49,40 @@ $("#back-to-top").click(function () {
 });
 
 /*--------------------------------------------------------------
-# Side nav
+# Side Navigation
 --------------------------------------------------------------*/
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "100px";
+  document.getElementById("sidenav").style.width = "100px";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("sidenav").style.width = "0";
 }
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.header.style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("sidenav").style.width = "250px";
+
+  document.getElementById("header").style.marginLeft = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("footer").style.marginLeft = "250px";
+
+  document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("main").style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("footer").style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.body.style.marginLeft = "0";
-  document.body.style.backgroundColor = "white";
+  document.getElementById("sidenav").style.width = "0px";
+
+  document.getElementById("header").style.marginLeft = "0px";
+  document.getElementById("main").style.marginLeft = "0px";
+  document.getElementById("footer").style.marginLeft = "0px";
+
+  document.getElementById("header").style.backgroundColor = "white";
+  document.getElementById("main").style.backgroundColor = "white)";
+  document.getElementById("footer").style.backgroundColor = "white";
 }
